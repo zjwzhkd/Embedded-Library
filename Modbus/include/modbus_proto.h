79d6006aae80325b2bb392ef79d93782c956e814
Keda Huang <huangkeda88@163.com>
2016-10-20 14:22:03 +0800

    Modbus: 添加功能码常量定义

--- Modbus/include/modbus_proto.h
+++ Modbus/include/modbus_proto.h
@@ -53,4 +53,25 @@ typedef enum
     MB_EX_GATEWAY_TARGET_DEVICE_FAILED_TO_RESPOND   = 0x0B
 } eMBException;
 
+/* Modbus从机功能码处理函数 */
+typedef eMBException (*MBSlaveFuncHandler) (uint8_t *pFrame, uint16_t *pLength);
+
+/* 常量定义 ------------------------------------------------------------------*/
+/* Modbus设备地址 */
+#define MB_ADDRESS_BROADCAST                    ( 0 )
+#define MB_ADDRESS_MIN                          ( 1 )
+#define MB_ADDRRSS_MAX                          ( 247 )
+
+/* Modbus功能码 */
+#define MB_FUNC_READ_COILS                      ( 0x01 )
+#define MB_FUNC_READ_DISCRETE_INPUTS            ( 0x02 )
+#define MB_FUNC_READ_HOLDING_REGISTERS          ( 0x03 )
+#define MB_FUNC_READ_INPUT_REGISTERS            ( 0x04 )
+#define MB_FUNC_WRITE_SINGLE_COIL               ( 0x05 )
+#define MB_FUNC_WRITE_SINGLE_REGISTER           ( 0x06 )
+#define MB_FUNC_WRITE_MULTIPLE_COILS            ( 0x0F )
+#define MB_FUNC_WRITE_MULTIPLE_REGISTERS        ( 0x10 )
+#define MB_FUNC_READWRITE_MULTIPLE_REGISTERS    ( 0x17 )
+#define MB_FUNC_ERROR                           ( 0x80 )
+
 #endif  /* __MODBUS_PROTO_H */
