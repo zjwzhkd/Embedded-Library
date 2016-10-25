256f144b4b8ab96082ce5a772078969b8f8b5ed5
Keda Huang <huangkeda88@163.com>
2016-10-25 20:08:58 +0800

    Modbus: API函数添加注释

--- Modbus/include/modbus.h
+++ Modbus/include/modbus.h
@@ -81,8 +81,51 @@ eMBError modbus_SlaveRegHoldingCB(uint8_t *regBuf, uint16_t startAddr,
  */
 eMBError modbus_SlaveRegInputCB(uint8_t *regBuf, uint16_t startAddr,
                                 uint16_t nRegs);
+
+/**
+ * Modbus Slave线圈读写回调函数
+ *
+ * @param regBuf: Modbus上层协议栈提供的缓存地址, 线圈状态对应着缓存的位域,
+ *                startAddr对应线圈的状态存储在regBuf[0]的LSB中, 当写入缓存时,
+ *                最后一个缓存字节中, 未使用的数据位需要保持为0
+ *
+ * @param startAddr: 回调函数需要处理的线圈的起始地址, 这里指协议地址(从0开始)
+ *
+ * @param nCoils: 回调函数需要处理的线圈的数量
+ *
+ * @param mode: MB_REG_READ  - 回调函数读取线圈, 将状态写入缓存
+ *              MB_REG_WRITE - 回调函数读取缓存, 并设置线圈的状态
+ *
+ * @return: MB_OK          - 没有错误发生
+ *          MB_ERR_NOREG   - 协议栈提供的地址范围超出应用层的处理范围,
+ *                           将导致MB_EX_ILLEGAL_DATA_ADDRESS异常响应
+ *          MB_ERR_TIMEOUT - 请求的寄存器还未完成准备, 需要延后处理,
+ *                           将导致MB_EX_SERVER_DEVICE_BUSY异常响应
+ *          MB_ERR_IO      - 发生了一个无法恢复的错误, 将导致
+ *                           MB_EX_SERVER_DEVICE_FAILURE异常响应
+ */
 eMBError modbus_SlaveRegCoilsCB(uint8_t *regBuf, uint16_t startAddr,
                                 uint16_t nCoils, eMBRegisterMode mode);
+
+/**
+ * Modbus Slave离散量输入读回调函数
+ *
+ * @param regBuf: Modbus上层协议栈提供的缓存地址, 离散量输入状态对应着缓存的位域,
+ *                startAddr对应离散量输入的状态存储在regBuf[0]的LSB中,
+ *                最后一个缓存字节中, 未使用的数据位保持为0
+ *
+ * @param startAddr: 回调函数需要处理的离散量输入的起始地址, 这里指协议地址(从0开始)
+ *
+ * @param nDiscrete: 回调函数需要处理的离散量输入的数量
+ *
+ * @return: MB_OK          - 没有错误发生
+ *          MB_ERR_NOREG   - 协议栈提供的地址范围超出应用层的处理范围,
+ *                           将导致MB_EX_ILLEGAL_DATA_ADDRESS异常响应
+ *          MB_ERR_TIMEOUT - 请求的寄存器还未完成准备, 需要延后处理,
+ *                           将导致MB_EX_SERVER_DEVICE_BUSY异常响应
+ *          MB_ERR_IO      - 发生了一个无法恢复的错误, 将导致
+ *                           MB_EX_SERVER_DEVICE_FAILURE异常响应
+ */
 eMBError modbus_SlaveRegDiscreteCB(uint8_t *regBuf, uint16_t startAddr,
                                    uint16_t nDiscrete);
 
